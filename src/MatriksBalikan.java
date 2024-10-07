9b3e5ec2897393f74f4b33f3d608c29b6143b0d2
unknown <naufarrelzhafifabhista@gmail.com>
2024-10-08 04:06:47 +0700

    feat: added MatriksBalikan's menu

--- src/MatriksBalikan.java
+++ src/MatriksBalikan.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
 public class MatriksBalikan {
+    static void Gauss(){
+
+    }
+
+    static void GaussJordan(){
+
+    }
+    
+    static void MatriksBalikan(){
+
+    }
+
+    static void Cramer(){
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Placeholder Matriks Balikan");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("MENU:");
+            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("2. Metode eliminasi Gauss-Jordan");
+            System.out.println("3. Metode matriks balikan");
+            System.out.println("4. Kaidah Cramer");
+            System.out.println("5. Keluar");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                //Selesai dipakai, kembali ke menu agar dapat digunakan kembali
+                case 1:
+                    MatriksBalikan.Gauss();
+                    return;
+                case 2:
+                    MatriksBalikan.GaussJordan();
+                    return;
+                case 3:
+                    MatriksBalikan.MatriksBalikan();
+                    return;
+                case 4:
+                    MatriksBalikan.Cramer();
+                    return;
+                case 5:
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Pilihan Invalid.");
+            }
+        }
     }
 }
