7722e98cff8fcde70c4a8c94b433b3c9205560d6
Inforable <hasri.fayadh@gmail.com>
2024-10-10 16:44:46 +0700

    update determinan

--- src/Determinan.java
+++ src/Determinan.java
@@ -1,48 +1,149 @@
 import java.util.Scanner;
-public class Determinan {
-    static void Gauss(){
 
+import org.w3c.dom.css.Counter;
+public class Determinan {
+    public static double getDeterminan(double[][] matrix, String function) {
+        double detResult = 1;
+        if (function.equals("gauss")) {
+            detResult = determinanGauss(matrix);
+            System.out.println("Determinan Matriks: ");
+            return detResult;
+        } 
+
+        if (function.equals("Kofaktor")) {
+            detResult = determinanKofaktor(matrix);
+            System.out.println("Determinan Matriks: ");
+            return detResult;
+        }
+        return detResult;
     }
 
-    static void GaussJordan(){
+    public static double determinanGauss(double[][] matrix) {
+        int n = matrix.length;
+        double[][] tmpMatrix = new double[n][n];
+
+        // Salin matriks ke tmpMatrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tmpMatrix[i][j] = matrix[i][j];
+            }
+        }
+        double determinan = 1;
+        int counterSwap = 0;
+
+        // Operasi Baris Elemneter (OBE)
+        for (int i = 0; i < n; i++) {
+            // Mencari elemen utama
+            double mainElement = tmpMatrix[i][i];
+            if (mainElement == 0) {
+                // Cari baris di bawah elemen utama yang tidak nol
+                boolean found = false;
+                for (int j = i + 1; j < n; j++) {
+                    if (tmpMatrix[j][i] != 0) {
+                        // Menukar baris
+                        swapRow(tmpMatrix, i, j);
+                        mainElement = tmpMatrix[i][i];
+                        found = true;
+                        CounterSwap++;
+                        break;
+                    }
+                }
+                
+            }
+        }
 
     }
     
-    static void MetodeKofaktor(){
-
-    }
-
-    static void MetodeSarrus(){
+    static void getKofaktor(){
 
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("MENU:");
-            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("\nMENU DETERMINAN:");
+            System.out.println("1. Metode Eliminasi Gauss");
             System.out.println("2. Metode Kofaktor");
             System.out.println("3. Keluar");
+            System.out.print("\nMasukkan pilihan: ");
+
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            int subChoice;
+            scanner.nextLine();
+
+            double[][] inputMatrix;
+            double determinant;
 
             switch (choice) {
-                //Selesai dipakai, kembali ke menu agar dapat digunakan kembali
                 case 1:
-                    Determinan.Gauss();
-                    return;
+                    Main.clearConsole();
+                    System.out.println("Metode Eliminasi Gauss\n");
+                    System.out.println("Pilihan Input");
+                    System.out.println("1. Keyboard");
+                    System.out.println("2. File");
+                    System.out.print("Masukkan pilihan: ");
+
+                    subChoice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (subChoice) {
+                        case 1:
+                            Main.clearConsole();
+                            System.out.println("Metode Eliminasi Gauss\n");
+                            inputMatrix = IOMatriks.getUserInput(scanner);
+                            determinant = getDeterminan(inputMatrix, "gauss");
+                            System.out.println("Determinan: " + determinant);
+                            break;
+                        case 2:
+                            Main.clearConsole();
+                            System.out.println("Metode Kofaktor\n");
+                            System.out.print("Masukkan file path: ");
+                            String filePath = scanner.nextLine();
+                            inputMatrix = IOMatriks.readFile(filePath);
+                            determinant = getDeterminan(inputMatrix, "gauss");
+                            System.out.println("Determinan: " + determinant);
+                            break;
+                        default:
+                            System.out.println("Pilihan Invalid!");
+                            break;
+                    }
+                    break;
                 case 2:
-                    Determinan.GaussJordan();
-                    return;
+                    Main.clearConsole();
+                    System.out.println("Metode Kofaktor\n");
+                    System.out.println("Pilihan Input");
+                    System.out.println("1. Keyboard");
+                    System.out.println("2. File");
+                    System.out.print("Masukkan pilihan: ");
+
+                    subChoice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (subChoice) {
+                        case 1:
+                            Main.clearConsole();
+                            System.out.println("Metode Kofaktor\n");
+                            inputMatrix = IOMatriks.getUserInput(scanner);
+                            determinant = getDeterminan(inputMatrix, "kofaktor");
+                            System.out.println("Determinan: " + determinant);
+                            break;
+                        case 2: 
+                            Main.clearConsole();
+                            System.out.println("Metode Kofaktor\n");
+                            System.out.print("Masukkan file path: ");
+                            String filePath = scanner.nextLine();
+                            inputMatrix = IOMatriks.readFile(filePath);
+                            determinant = getDeterminan(inputMatrix, "kofaktor");
+                            System.out.println("Determinan: " + determinant);
+                            break;
+                        default:
+                            System.out.println("Pilihan Invalid!");
+                            break;
+                    }
+                    break;
                 case 3:
-                    Determinan.MetodeKofaktor();
-                    return;
-                case 4:
-                    Determinan.MetodeSarrus();
-                    return;
-                case 5:
-                    scanner.close();
-                    return;
+                    Main.clearConsole();
+                    System.out.println("Kembali ke Menu Utama.");
                 default:
                     System.out.println("Pilihan Invalid.");
             }
