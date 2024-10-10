62a877d8da8a26ff2afac0dd0615bc174d112f3a
I Made Wiweka Putera <78787384+wiwekaputera@users.noreply.github.com>
2024-10-10 16:31:41 +0700

    integrate OBE invers to OBE

--- src/MatriksBalikan.java
+++ src/MatriksBalikan.java
@@ -22,7 +22,7 @@ public class MatriksBalikan {
                 augmentedMatrix[i][j] = (i == j - n) ? 1 : 0;
             }
         }
-        augmentedMatrix = OBEInvers.toReducedRowEchelon(augmentedMatrix);
+        augmentedMatrix = OBE.toReducedRowEchelon(augmentedMatrix);
         double[][] matriksInverse = new double[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
