865f43037454df292901ba7858f67d30ba8f98b3
unknown <naufarrelzhafifabhista@gmail.com>
2024-10-15 15:01:45 +0700

    feat: tested 2 different Linear Regression algorithm

--- src/RegresiBerganda.java
+++ src/RegresiBerganda.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
+import javax.crypto.SealedObject;
 public class RegresiBerganda {
-    public static double[][][] multipleLinearRegression(double[][] X, double[][] Y){
+    public static double[] multipleLinearRegression1(double[][] X, double[][] Y){
         //Y = XB + e; Ycap = XB; e = Y - Ycap
-        //B = (Xt.X)^-1 . Xt . Y
+        //(Xt.X)(B) = Xt.Y
+        //ukuran Y: m x 1
+        //ukuran X: m x n
+        //ukuran B: n x 1
+        //ukuran e: m x 1
+        double[][] XT = OBE.transpose(X);
+        double[][] XTX = OBE.multiplyMatrix(XT, X);
+        double[][] XTY = OBE.multiplyMatrix(XT, Y);
+
+        //Finding B
+        double[] B = SPL.gauss(OBE.toAugmented(XTX, XTY));
+        return B;
+    }
+
+    public static double[][] multipleLinearRegression2(double[][] X, double[][] Y){
+        //Y = XB + e; Ycap = XB; e = Y - Ycap
+        //(Xt.X)(B) = Xt.Y
         //ukuran Y: m x 1
         //ukuran X: m x n
         //ukuran B: n x 1
@@ -15,13 +32,37 @@ public class RegresiBerganda {
         //Finding B
         double[][] B = OBE.multiplyMatrix(XTXInvXT, Y);
 
-        //Finding e
-        double [][] Ycap = OBE.multiplyMatrix(X, B);
-        double [][] e = OBE.substractMatrix(Y, Ycap);
-        return new double[][][]{B, e};
+        // //Finding e
+        // double [][] Ycap = OBE.multiplyMatrix(X, B);
+        // double [][] e = OBE.substractMatrix(Y, Ycap);
+        return B;
     }
 
     public static void main(String[] args) {
+        int i;
+        double[][] X = 
+        {{4},
+        {4.5},
+        {5},
+        {5.5},
+        {6},
+        {6.5},
+        {7}
+        };
+        double[][] Y = 
+        {{3},
+        {42},
+        {45},
+        {51},
+        {53},
+        {61},
+        {62}
+        };
+        IOMatriks.writeMatrix(multipleLinearRegression2(X, Y));
+        double[] B = multipleLinearRegression1(X, Y);
+        for (i = 0; i < B.length; i++){
+            System.out.print(B[i] + " ");
+        }
         // Scanner scanner = new Scanner(System.in);
         // while (true) {
         //     System.out.println("MENU:");
