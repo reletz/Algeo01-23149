6b688b222034f0582c998aac8307cf9afb7fe173
unknown <naufarrelzhafifabhista@gmail.com>
2024-10-15 16:02:28 +0700

    fix: adjust Linear Regression algorithm to match Gauss Elimination

--- src/RegresiBerganda.java
+++ src/RegresiBerganda.java
@@ -2,55 +2,36 @@ import java.util.Scanner;
 import javax.crypto.SealedObject;
 public class RegresiBerganda {
     public static double[] multipleLinearRegression1(double[][] X, double[][] Y){
-        //Y = XB + e; Ycap = XB; e = Y - Ycap
         //(Xt.X)(B) = Xt.Y
         //ukuran Y: m x 1
-        //ukuran X: m x n
-        //ukuran B: n x 1
+        //ukuran X: m x n (Ada (n - 1) data)
+        //ukuran B: m x 1
         //ukuran e: m x 1
         double[][] XT = OBE.transpose(X);
         double[][] XTX = OBE.multiplyMatrix(XT, X);
+        IOMatriks.writeMatrix(XTX);
         double[][] XTY = OBE.multiplyMatrix(XT, Y);
+        IOMatriks.writeMatrix(XTY);
 
         //Finding B
         double[] B = SPL.gauss(OBE.toAugmented(XTX, XTY));
         return B;
     }
 
-    public static double[][] multipleLinearRegression2(double[][] X, double[][] Y){
-        //Y = XB + e; Ycap = XB; e = Y - Ycap
-        //(Xt.X)(B) = Xt.Y
-        //ukuran Y: m x 1
-        //ukuran X: m x n
-        //ukuran B: n x 1
-        //ukuran e: m x 1
-        double[][] XT = OBE.transpose(X);
-        double[][] XTX = OBE.multiplyMatrix(XT, X);
-        double[][] XTXInv = MatriksBalikan.inversBalikan(XTX);
-        double[][] XTXInvXT = OBE.multiplyMatrix(XTXInv, XT);
-
-        //Finding B
-        double[][] B = OBE.multiplyMatrix(XTXInvXT, Y);
-
-        // //Finding e
-        // double [][] Ycap = OBE.multiplyMatrix(X, B);
-        // double [][] e = OBE.substractMatrix(Y, Ycap);
-        return B;
-    }
-
     public static void main(String[] args) {
+        //Example
         int i;
         double[][] X = 
-        {{4},
-        {4.5},
-        {5},
-        {5.5},
-        {6},
-        {6.5},
-        {7}
+        {{1, 4},
+        {1, 4.5},
+        {1, 5},
+        {1, 5.5},
+        {1, 6},
+        {1, 6.5},
+        {1, 7}
         };
         double[][] Y = 
-        {{3},
+        {{33},
         {42},
         {45},
         {51},
@@ -58,7 +39,6 @@ public class RegresiBerganda {
         {61},
         {62}
         };
-        IOMatriks.writeMatrix(multipleLinearRegression2(X, Y));
         double[] B = multipleLinearRegression1(X, Y);
         for (i = 0; i < B.length; i++){
             System.out.print(B[i] + " ");
